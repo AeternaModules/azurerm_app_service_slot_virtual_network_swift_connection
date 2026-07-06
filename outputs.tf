a36c4ae77b1714c13bff6c540ed7631a49961b44
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "app_service_slot_virtual_network_swift_connections" {
   description = "All app_service_slot_virtual_network_swift_connection resources"
   value       = azurerm_app_service_slot_virtual_network_swift_connection.app_service_slot_virtual_network_swift_connections
 }
+output "app_service_slot_virtual_network_swift_connections_app_service_id" {
+  description = "List of app_service_id values across all app_service_slot_virtual_network_swift_connections"
+  value       = [for k, v in azurerm_app_service_slot_virtual_network_swift_connection.app_service_slot_virtual_network_swift_connections : v.app_service_id]
+}
+output "app_service_slot_virtual_network_swift_connections_slot_name" {
+  description = "List of slot_name values across all app_service_slot_virtual_network_swift_connections"
+  value       = [for k, v in azurerm_app_service_slot_virtual_network_swift_connection.app_service_slot_virtual_network_swift_connections : v.slot_name]
+}
+output "app_service_slot_virtual_network_swift_connections_subnet_id" {
+  description = "List of subnet_id values across all app_service_slot_virtual_network_swift_connections"
+  value       = [for k, v in azurerm_app_service_slot_virtual_network_swift_connection.app_service_slot_virtual_network_swift_connections : v.subnet_id]
+}
 
